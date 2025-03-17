a3b1f48d1fbf36e054ea4cf6ce84ec4862013d7d
Vaibhav Pratap Singh Kushwah <137618886+Volpes12@users.noreply.github.com>
2025-03-17 13:59:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Scroll Reveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal(`.container , .mataji , .out` , {
+    interval: 200
+})
